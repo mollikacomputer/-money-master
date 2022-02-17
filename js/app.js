3de20967373d188d9deb924a60baa03b604c2d91
mollikacomputer <mollikacomputer3@gmail.com>
2022-02-17 11:05:28 +0600

    added remaining balance have some problem NaN

--- js/app.js
+++ js/app.js
@@ -4,6 +4,15 @@
         // console.log(amount);
         return amount;
     }
+    // saving percentence
+    function getSaving(){
+        const balanceField = document.getElementById('balance');
+        let balance = balanceField.innerText;
+        const saveAmount = getInputNumber('saving');
+        const save = saveAmount.value;
+        const remainingBalance = balance / save;
+        return remainingBalance;
+    }
 
     document.getElementById('calculate-button').addEventListener('click', function(){
         // income amount
@@ -13,6 +22,7 @@
         const food = getInputNumber('food');
         const rent = getInputNumber('rent');
         const clothes = getInputNumber('clothes');
+
         // total expenses
         const totalExpenses = food + rent + clothes;
         // console.log(totalExpenses);
@@ -20,8 +30,17 @@
         totalExpensesField.innerText = parseInt(totalExpenses);
 
         // balance after expenses
+        
         const balanceField = document.getElementById('balance');
         let balance = balanceField.innerText;
         balance = parseInt(income) - parseInt(totalExpenses);
         balanceField.innerText = balance;
+
     })
+    document.getElementById('save-button').addEventListener('click', function(){
+        //remaining balance
+        const remainingBalanceField = document.getElementById('remaining-balance-field');
+        remainingBalanceField.innerText = getSaving();
+        
+    })
+
